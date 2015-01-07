71b365ce9ce0dbf650c92592e34ef184c4da32c6
Ryota Arai <ryota.arai@gmail.com>
2015-01-07 18:15:41 +0900

    Update.

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'omnibus', '~> 4.0.0'
 
 # Use Chef's software definitions. It is recommended that you write your own
 # software definitions, but you can clone/fork Chef's to get you started.
-gem 'omnibus-software', github: 'ryotarai/omnibus-software', branch: 'gnu-mirror'
+gem 'omnibus-software', github: 'opscode/omnibus-software'
 
 # Use Test Kitchen with Vagrant for converging the build environment
 gem 'test-kitchen',    '~> 1.2'
--- scripts/build_with_docker_ubuntu_14.04.sh
+++ scripts/build_with_docker_ubuntu_14.04.sh
@@ -2,11 +2,9 @@
 set -ex
 
 DIR=$(dirname $(cd $(dirname $0); pwd))
-IMAGE_NAME=omnibus-itamae-ubuntu-14-04
 
-if ! (docker images | grep -q omnibus-itamae-ubuntu-14-04); then
-  CID=$(docker run -d -v $DIR:/app ubuntu:14.04 /bin/bash -ex -c \
-  'apt-get update
+docker run -d -v $DIR:/app ubuntu:14.04 /bin/bash -ex -c \
+'apt-get update
 apt-get install -y software-properties-common git-core build-essential
 apt-add-repository -y ppa:brightbox/ruby-ng
 apt-get update
@@ -16,13 +14,5 @@ cd /app
 git config --global user.email "ryota.arai+itamae@gmail.com"
 git config --global user.name "Itamae Builder"
 bundle install -j4
-')
-  echo "Creating base image..."
-  docker wait $CID
-  IID=$(docker commit $CID)
-  docker tag $IID $IMAGE_NAME
-fi
-
-docker run -e GNU_ARCHIVE_MIRROR=$GNU_ARCHIVE_MIRROR -v $DIR:/app $IMAGE_NAME /bin/bash -ex -c \
-'cd /app
 bundle exec omnibus build itamae'
+
