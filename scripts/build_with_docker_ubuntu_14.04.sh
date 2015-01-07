2c1046ed552f3371f9bafa249681f046d6d87894
Ryota Arai <ryota.arai@gmail.com>
2015-01-07 11:12:05 +0900

    Fix.

--- scripts/build_with_docker_ubuntu_14.04.sh
+++ scripts/build_with_docker_ubuntu_14.04.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 DIR=$(dirname $(cd $(dirname $0); pwd))
 docker run -v $DIR:/app ubuntu:14.04 /bin/bash -ex -c \
-'git config --global user.email "ryota.arai+itamae@gmail.com"
-git config --global user.name "Itamae Builder"
-apt-get update
+'apt-get update
 apt-get install -y software-properties-common git-core build-essential
 apt-add-repository -y ppa:brightbox/ruby-ng
 apt-get update
 apt-get install -y ruby2.2 ruby2.2-dev
 gem install bundler
 cd /app
+git config --global user.email "ryota.arai+itamae@gmail.com"
+git config --global user.name "Itamae Builder"
 bundle install -j4
 bundle exec omnibus build itamae'
