1ba1818ff1268a9a2e142f4393dc6f25abf1f634
Ryota Arai <ryota.arai@gmail.com>
2015-01-20 00:14:59 +0900

    Update build_with_docker_ubuntu_14.04.sh

--- scripts/build_with_docker_ubuntu_14.04.sh
+++ scripts/build_with_docker_ubuntu_14.04.sh
@@ -3,16 +3,10 @@ set -ex
 
 DIR=$(dirname $(cd $(dirname $0); pwd))
 
-docker run -v $DIR:/app ubuntu:14.04 /bin/bash -ex -c \
-'apt-get update
-apt-get install -y software-properties-common git-core build-essential autoconf
-apt-add-repository -y ppa:brightbox/ruby-ng
-apt-get update
-apt-get install -y ruby2.2 ruby2.2-dev
-gem install bundler
-cd /app
-git config --global user.email "ryota.arai+itamae@gmail.com"
+docker run -v $DIR:/app ryotarai/omnibus-docker-image /bin/bash -ex -c \
+'git config --global user.email "ryota.arai+itamae@gmail.com"
 git config --global user.name "Itamae Builder"
+cd /app
 bundle install -j4
 bundle exec omnibus build itamae'
 
