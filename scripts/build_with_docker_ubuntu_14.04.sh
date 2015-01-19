bc0943ab36dff0a54c0e77b6a0ca3dda7c9171af
Ryota Arai <ryota.arai@gmail.com>
2015-01-19 23:54:11 +0900

    Update build_with_docker_ubuntu_14.04.sh

--- scripts/build_with_docker_ubuntu_14.04.sh
+++ scripts/build_with_docker_ubuntu_14.04.sh
@@ -5,7 +5,7 @@ DIR=$(dirname $(cd $(dirname $0); pwd))
 
 docker run -v $DIR:/app ubuntu:14.04 /bin/bash -ex -c \
 'apt-get update
-apt-get install -y software-properties-common git-core build-essential
+apt-get install -y software-properties-common git-core build-essential autoconf
 apt-add-repository -y ppa:brightbox/ruby-ng
 apt-get update
 apt-get install -y ruby2.2 ruby2.2-dev
