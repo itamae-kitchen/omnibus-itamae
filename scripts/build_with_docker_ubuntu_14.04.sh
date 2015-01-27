31a7360f8f2c50ceb2325ae86526d1b4a721d52b
Ryota Arai <ryota.arai@gmail.com>
2015-01-27 16:32:34 +0900

    Update docker image name.

--- scripts/build_with_docker_ubuntu_14.04.sh
+++ scripts/build_with_docker_ubuntu_14.04.sh
@@ -3,7 +3,7 @@ set -ex
 
 DIR=$(dirname $(cd $(dirname $0); pwd))
 
-docker run -v $DIR:/app ryotarai/omnibus-docker-image /bin/bash -ex -c \
+docker run -v $DIR:/app ryotarai/omnibus-docker-trusty /bin/bash -ex -c \
 'git config --global user.email "ryota.arai+itamae@gmail.com"
 git config --global user.name "Itamae Builder"
 cd /app
