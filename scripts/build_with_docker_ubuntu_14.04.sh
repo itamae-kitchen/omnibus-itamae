a419fc8a9a77fd8afe827f144864414a42cf020d
Ryota Arai <ryota-arai@cookpad.com>
2015-03-02 17:16:15 +0900

    Revert "Use system gecode."
    This reverts commit 2bf65cf42359a69f654670536efc40821c6e6026.

--- scripts/build_with_docker_ubuntu_14.04.sh
+++ scripts/build_with_docker_ubuntu_14.04.sh
@@ -4,10 +4,9 @@ set -ex
 DIR=$(dirname $(cd $(dirname $0); pwd))
 
 docker run -v $DIR:/app ryotarai/omnibus:trusty /bin/bash -ex -c \
-'apt-get -y install libgecode-dev
-git config --global user.email "ryota.arai+itamae@gmail.com"
+'git config --global user.email "ryota.arai+itamae@gmail.com"
 git config --global user.name "Itamae Builder"
 cd /app
-USE_SYSTEM_GECODE=1 bundle install -j4
+bundle install -j4
 bundle exec omnibus build itamae'
 
