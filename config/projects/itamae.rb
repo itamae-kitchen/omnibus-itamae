cf75cbc90be8f345fae2b2daa091d311496da412
Ryota Arai <ryota.arai@gmail.com>
2015-03-23 16:13:10 +0900

    Install specinfra-ec2_metadata-tags.

--- config/projects/itamae.rb
+++ config/projects/itamae.rb
@@ -6,13 +6,14 @@ install_dir '/opt/itamae'
 build_version do
   source :version, from_dependency: 'itamae'
 end
-build_iteration 1
+build_iteration 2
 
 # creates required build directories
 dependency "preparation"
 
 # itamae dependencies/components
 dependency 'itamae'
+dependency 'specinfra-ec2_metadata-tags'
 override 'ruby', version: '2.1.5'
 
 # version manifest file
---
+++ config/software/specinfra-ec2_metadata-tags.rb
@@ -0,0 +1,13 @@
+name "specinfra-ec2_metadata-tags"
+default_version "0.0.4"
+
+dependency "ruby"
+dependency "rubygems"
+
+env = {
+  "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}"
+}
+
+build do
+  gem "install specinfra-ec2_metadata-tags --no-ri --no-rdoc -v #{version}", env: env
+end
