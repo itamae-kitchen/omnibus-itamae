3222efec4b2b8b0965f7d810681d5a77e13838f3
Ryota Arai <ryota.arai@gmail.com>
2015-10-06 17:01:41 +0900

    Upload packages to S3.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 umask 0066
 
 WORKDIR="$(cd $(dirname $(dirname $0)); pwd)"
@@ -6,15 +7,19 @@ cd $WORKDIR
 
 gpg -o tmp/E6506E2E.key --export-secret-keys E6506E2E
 
+aws s3 sync s3://packages.itamae.kitchen/ static
+
 for dist in trusty; do
     echo "Building a deb package for ${dist}..."
     docker build -t omnibus-itamae-builder:$dist -f dockerfiles/$dist .
-    docker run -v $(pwd):/work -it omnibus-itamae-builder:$dist /bin/bash -c "
+    docker run --rm -v $(pwd):/work -it omnibus-itamae-builder:$dist /bin/bash -c "
         cd /work && \
         gpg --import tmp/E6506E2E.key && \
         bundle exec omnibus build itamae && \
-        cd deb && \
-        reprepro includedeb trusty ../pkg/*.deb
+        cd static/deb && \
+        reprepro includedeb trusty ../../pkg/*.deb
     "
     rm -f pkg/*
 done
+
+aws s3 sync static s3://packages.itamae.kitchen/ --grants 'read=uri=http://acs.amazonaws.com/groups/global/AllUsers'
